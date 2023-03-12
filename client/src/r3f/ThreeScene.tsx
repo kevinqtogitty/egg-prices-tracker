6a8daeb6c4b5b0e6c72f608cc18928ce79b08172
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-03-12 18:30:21 -0400

    Update: added pointlight

--- client/src/r3f/ThreeScene.tsx
+++ client/src/r3f/ThreeScene.tsx
@@ -2,14 +2,21 @@ import { OrbitControls, OrthographicCamera } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import React from 'react';
 import { Eggs } from './3dModel';
+import { EggModel } from './EggModel';
 
 const ThreeScene = () => {
   return (
-    <Canvas>
+    <Canvas shadows={true}>
       <OrbitControls />
       <OrthographicCamera makeDefault zoom={425} position={[4, 5, 7]} />
       <ambientLight />
-      <Eggs />
+      <EggModel />
+      <pointLight
+        position={[2, 3, 0]}
+        castShadow
+        shadow-mapSize-height={512}
+        shadow-mapSize-width={512}
+      />
     </Canvas>
   );
 };
